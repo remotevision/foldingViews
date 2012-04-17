0c667a07c81ec4817dc21948c7002fc1939c8cb7
Ryan Kelly <ryank@remotevisioninc.com>
2012-04-17 18:33:13 -0500

    Initial Commit

---
+++ foldingView/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  foldingView
+//
+//  Created by Ryan Kelly on 4/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
